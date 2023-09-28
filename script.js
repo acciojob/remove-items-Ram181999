d4ffc8f0a532fd1bbfd7f4998a249651132c2021
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-28 22:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// Function to remove the selected color
+function removeColor() {
+    const colorSelect = document.getElementById("colorSelect");
+
+    // Get the selected index
+    const selectedIndex = colorSelect.selectedIndex;
+
+    if (selectedIndex >= 0) {
+        // Remove the selected option
+        colorSelect.remove(selectedIndex);
+    }
+}
+
+// Add a click event listener to the button
+const button = document.querySelector('input[type="button"]');
+button.addEventListener('click', removeColor);
+
